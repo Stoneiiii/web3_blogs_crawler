308ceb6f04d7aceb783b27a1730b9c7de1824aac
lei hu <513321195@qq.com>
2024-10-29 17:59:59 -0700

    front-end add shadcn ui

--- src/components/BlogsTable.tsx
+++ src/components/BlogsTable.tsx
@@ -1,4 +1,13 @@
 import { BlogEntryType } from "./graphql/GetAllBlogs";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 interface BlogTableProps {
   entries: BlogEntryType[];
@@ -6,30 +15,29 @@ interface BlogTableProps {
 
 export function BlogsTable({ entries }: BlogTableProps) {
   return (
-    <table className="blog-table">
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Content</th>
-          <th>Time</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
+    <Table className="blog-table">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="text-center w-[400px]">Title</TableHead >
+          <TableHead className="text-center ">Description</TableHead >
+          <TableHead className="text-center">Time</TableHead >
+          <TableHead className="text-center">URL</TableHead >
+        </TableRow>
+      </TableHeader>
+      <TableBody>
         {entries.map((entry) => (
-          <tr key={entry.id}>
-            <td>{entry.title}</td>
-            <td>{entry.description}</td>
-            <td>{entry.time}</td>
-            <td>
+          <TableRow key={entry.id}>
+            <TableCell className="text-center ">{entry.title}</TableCell >
+            <TableCell >{entry.description}</TableCell >
+            <TableCell >{new Date(entry.time).toLocaleDateString()}</TableCell >
+            <TableCell >
               <a href={entry.url} target="_blank" rel="noopener noreferrer">
                 Link
               </a>
-            </td>
-          </tr>
+            </TableCell >
+          </TableRow>
         ))}
-      </tbody>
-    </table>
+      </TableBody>
+    </Table>
   );
 }
